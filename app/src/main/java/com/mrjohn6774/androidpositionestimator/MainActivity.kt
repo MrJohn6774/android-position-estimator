aca44b7a593dddd28633fb1361876a4d791c7bf4
MrJohn6774 <45367030+MrJohn6774@users.noreply.github.com>
2023-11-25 11:37:13 +1100

    hide system ui

--- app/src/main/java/com/mrjohn6774/androidpositionestimator/MainActivity.kt
+++ app/src/main/java/com/mrjohn6774/androidpositionestimator/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.mrjohn6774.androidpositionestimator
 
+import android.annotation.SuppressLint
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.WindowManager.LayoutParams
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import android.app.NativeActivity
 
 class MainActivity : NativeActivity() {
@@ -12,5 +20,28 @@ class MainActivity : NativeActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // This will put the game behind any cutouts and waterfalls on devices which have
+        // them, so the corresponding insets will be non-zero.
+
+        // We cannot guarantee that AndroidManifest won't be tweaked
+        // and we don't want to crash if that happens so we suppress warning.
+        @SuppressLint("ObsoleteSdkInt")
+        if (VERSION.SDK_INT >= VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS
+        }
+        val decorView: View = window.decorView
+        val controller = WindowInsetsControllerCompat(
+            window,
+            decorView
+        )
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.hide(WindowInsetsCompat.Type.displayCutout())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
